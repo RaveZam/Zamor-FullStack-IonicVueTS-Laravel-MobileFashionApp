dea5310242c3772b58300825011852248c23ec9e
Duke <114975455+RaveZam@users.noreply.github.com>
2025-02-14 20:59:17 +0800

    added scrolling images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,22 @@
 <template>
   <ion-page>
-    <div>
-      <ion-toolbar class="border-0">
-        <div class="flex justify-between items-center my-4">
-          <ion-icon
-            @click="logMsg()"
-            name="settings-outline"
-            class="text-3xl mx-4"
-          ></ion-icon>
-          <span class="font-bermirs text-[2.2rem] font-[500]">Zamor</span>
-          <ion-icon
-            name="notifications-outline"
-            class="text-3xl mx-4"
-          ></ion-icon>
-        </div>
-      </ion-toolbar>
-    </div>
     <ion-content>
+      <div>
+        <ion-toolbar class="border-0">
+          <div class="flex justify-between items-center">
+            <ion-icon
+              @click="logMsg()"
+              name="settings-outline"
+              class="text-3xl mx-4"
+            ></ion-icon>
+            <span class="font-bermirs text-[2.2rem] font-[500]">Zamor</span>
+            <ion-icon
+              name="notifications-outline"
+              class="text-3xl mx-4"
+            ></ion-icon>
+          </div>
+        </ion-toolbar>
+      </div>
       <div class="mx-4 mt-4 flex flex-col -space-y-2">
         <span class="font-robotoTitle">For You</span>
         <span class="font-roboto opacity-80">Explore Brands To your taste</span>
@@ -27,7 +27,7 @@
       <div class="grid grid-cols-2">
         <div
           class="font-roboto"
-          v-for="(productCard, index) in mockUpDBProducts"
+          v-for="(productCard, index) in limitedProducts"
           :key="index"
         >
           <div class="p-2 relative">
@@ -52,7 +52,8 @@
         </div>
       </div>
       <div>
-        <div class="flex whitespace-nowrap animate-scroll mt-4">
+        <div class="flex whitespace-nowrap animate-scroll">
+          <h1 class="font-bermirs mx-8">Featured</h1>
           <h1 class="font-bermirs mx-8">Featured</h1>
           <h1 class="font-bermirs mx-8">Featured</h1>
           <h1 class="font-bermirs mx-8">Featured</h1>
@@ -60,23 +61,71 @@
           <h1 class="font-bermirs mx-8">Featured</h1>
         </div>
 
-        <div class="w-screen overflow-x-auto">
-          <div class="flex min-w-max flex-row">
-            <div
-              class="p-8"
-              v-for="(ForYou, index) in mockUpDBProducts"
-              :key="index"
-            >
-              <div class="py-4">
-                <div>
-                  <ion-img
-                    :src="ForYou.productThumbnail"
-                    class="max-w-screen"
-                  ></ion-img>
-                </div>
-              </div>
+        <Swiper
+          ref="swiperRef"
+          :modules="[Autoplay, Pagination]"
+          :slides-per-view="1"
+          :space-between="10"
+          :loop="true"
+          :autoplay="{ delay: 3000, disableOnInteraction: false }"
+          class="w-full max-w-[500px]"
+        >
+          <SwiperSlide
+            v-for="(item, index) in duplicatedSlides"
+            :key="index"
+            class="flex flex-row items-center p-8 pb-4 bg-gray-50"
+          >
+            <img
+              :src="item.productThumbnail"
+              :alt="item.productName"
+              class="object-cover mb-2"
+            />
+            <p class="text-lg font-bold">{{ item.brandName }}</p>
+            <p class="text-md">{{ item.productName }}</p>
+            <p class="text-sm text-gray-600">
+              ₱{{ item.productPrice.toLocaleString() }}
+            </p>
+          </SwiperSlide>
+        </Swiper>
 
-              <h1 class="font-bermirs">Explore Brands</h1>
+        <div class="flex whitespace-nowrap animate-scroll">
+          <h1 class="font-bermirs mx-8">Explore</h1>
+          <h1 class="font-bermirs mx-8">Explore</h1>
+          <h1 class="font-bermirs mx-8">Explore</h1>
+          <h1 class="font-bermirs mx-8">Explore</h1>
+          <h1 class="font-bermirs mx-8">Explore</h1>
+          <h1 class="font-bermirs mx-8">Explore</h1>
+          <h1 class="font-bermirs mx-8">Explore</h1>
+          <h1 class="font-bermirs mx-8">Explore</h1>
+          <h1 class="font-bermirs mx-8">Explore</h1>
+        </div>
+
+        <div class="grid grid-cols-2">
+          <div
+            class="font-roboto"
+            v-for="(productCard, index) in remainingProducts"
+            :key="index"
+          >
+            <div class="p-2 relative">
+              <div
+                class="absolute right-1 mr-4 mt-4 bg-white px-4 pt-4 pb-2 rounded-md opacity-70"
+              >
+                <ion-icon name="heart-outline" class="text-2xl"></ion-icon>
+              </div>
+              <ion-img class="py-2" :src="productCard.productThumbnail">
+              </ion-img>
+
+              <div>
+                <div class="flex justify-between">
+                  <span>{{ productCard.brandName }}</span>
+                  <span class="font-roboto">
+                    ₱{{ productCard.productPrice }}</span
+                  >
+                </div>
+                <span class="font-robotoMed">{{
+                  productCard.productName
+                }}</span>
+              </div>
             </div>
           </div>
         </div>
@@ -86,6 +135,11 @@
 </template>
 
 <script setup lang="ts">
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
 import {
   IonPage,
   IonHeader,
@@ -95,7 +149,7 @@ import {
   IonIcon,
   IonImg,
 } from "@ionic/vue";
-import { ref } from "vue";
+import { computed, nextTick, onMounted, onUnmounted, ref } from "vue";
 
 function logMsg() {
   console.log("Clicked");
@@ -107,6 +161,44 @@ type productCardTypes = {
   productPrice: number;
   productName: string;
 };
+type featuredCardTypes = {
+  productThumbnail: string;
+  brandName: string;
+  productPrice: number;
+  productName: string;
+};
+
+const mockupDBFeatured = ref<featuredCardTypes[]>([
+  {
+    productThumbnail: "./images/Clothings/FadedCityGraphicClassicTshirt.webp",
+    brandName: "Kalvin Klein",
+    productPrice: 12000,
+    productName: "Faded City Graphic Classic Shirt",
+  },
+  {
+    productThumbnail: "./images/Clothings/OriginalStraightDenimJacket.webp",
+    brandName: "Kalvin Klein",
+    productPrice: 12000,
+    productName: "Original Straight Denim Jacket",
+  },
+  {
+    productThumbnail: "./images/Clothings/OriginalStraightFitJeans.webp",
+    brandName: "Kalvin Klein",
+    productPrice: 12000,
+    productName: "Original Straight Fit Jeans",
+  },
+  {
+    productThumbnail: "./images/Clothings/OriginalStraightFitJeans.webp",
+    brandName: "Kalvin Klein",
+    productPrice: 12000,
+    productName: "Original Straight Fit Jeans",
+  },
+]);
+
+const duplicatedSlides = computed(() => [
+  ...mockupDBFeatured.value,
+  ...mockupDBFeatured.value,
+]);
 
 const mockUpDBProducts = ref<productCardTypes[]>([
   {
@@ -133,17 +225,20 @@ const mockUpDBProducts = ref<productCardTypes[]>([
     productPrice: 24000,
     productName: "Light Blue Slim Jacket",
   },
-  // {
-  //   productThumbnail: "./images/Clothings/LongCoat.webp",
-  //   brandName: "Kalvin Klein",
-  //   productPrice: 2000,
-  //   productName: "Long Coat",
-  // },
-  // {
-  //   productThumbnail: "./images/Clothings/SlimLeggings.webp",
-  //   brandName: "Kalvin Klein",
-  //   productPrice: 24000,
-  //   productName: "SlimLeggings",
-  // },
+  {
+    productThumbnail: "./images/Clothings/LongCoat.webp",
+    brandName: "Kalvin Klein",
+    productPrice: 2000,
+    productName: "Long Coat",
+  },
+  {
+    productThumbnail: "./images/Clothings/SlimLeggings.webp",
+    brandName: "Kalvin Klein",
+    productPrice: 24000,
+    productName: "SlimLeggings",
+  },
 ]);
+
+const limitedProducts = computed(() => mockUpDBProducts.value.slice(0, 4));
+const remainingProducts = computed(() => mockUpDBProducts.value.slice(4));
 </script>
